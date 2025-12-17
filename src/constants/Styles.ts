e4c7f048e65569b6735b35c75d279033a90d8356
HamidAliSE <hamidalig942@gmail.com>
2025-12-17 20:04:54 +0500

    add: common styling

---
+++ src/constants/Styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    heading: {
+        fontSize: 24,
+    },
+    subheading: {
+        fontSize: 20,
+    },
+    title: {
+        fontSize: 16,
+    },
+    caption: {
+        fontSize: 12,
+    },
+    screenPadding: {
+        paddingHorizontal: 20,
+    },
+});
