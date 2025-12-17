e0229eef827565124190f64b52e6e3ea93121b2f
HamidAliSE <hamidalig942@gmail.com>
2025-12-17 20:05:17 +0500

    add: app colors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,10 @@
+export default {
+    primary: 'rgba(0, 79, 147, 1)',
+    primaryLight: 'rgba(14, 97, 168, 1)',
+    secondary: 'rgba(239, 84, 90, 1)',
+    secondaryLight: 'rgba(255, 188, 190, 1)',
+    white: 'rgba(255, 255, 255, 1)',
+    black: 'rgba(0, 0, 0, 1)',
+    lighterGrey: 'rgba(230, 238, 246, 1)',
+    transparent: 'rgba(0, 0, 0, 0)',
+};
